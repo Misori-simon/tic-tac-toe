0590dee0303dcf12f7cf86d2186a9cb7189aea02
Misori-simon <misorisimon@gmail.com>
2020-11-05 15:50:21 +0100

    Debug, add tie condition, add game_over method -added condition for a game draw -fix some variable scope errors -add a game over method

--- lib/logic.rb
+++ lib/logic.rb
@@ -12,27 +12,26 @@ class Player
 end
 
 class Game
-  @grid = [
-    %w[a1 a2 a3],
-    %w[b1 b2 b3],
-    %w[c1 c2 c3]
-  ]
-  @valid_moves = %w[a1 a2 a3 b1 b2 b3 c1 c2 c3]
-  # @turn = 1
-  @winning_cases = [
-    Set.new(@grid[0]),
-    Set.new(@grid[1]),
-    Set.new(@grid[2]),
-    Set.new([@grid[0][0], @grid[1][0], @grid[2][0]]),
-    Set.new([@grid[0][1], @grid[1][1], @grid[2][1]]),
-    Set.new([@grid[0][2], @grid[1][2], @grid[2][2]]),
-    Set.new([@grid[0][0], @grid[1][1], @grid[2][2]]),
-    Set.new([@grid[0][2], @grid[1][1], @grid[2][0]])
-  ]
-
   def initialize(player_one, player_two)
     @player_one = player_one
     @player_two = player_two
+    @valid_moves = %w[a1 a2 a3 b1 b2 b3 c1 c2 c3]
+    @turn = 1
+    @grid = [
+      %w[a1 a2 a3],
+      %w[b1 b2 b3],
+      %w[c1 c2 c3]
+    ]
+    @winning_cases = [
+      Set.new(@grid[0]),
+      Set.new(@grid[1]),
+      Set.new(@grid[2]),
+      Set.new([@grid[0][0], @grid[1][0], @grid[2][0]]),
+      Set.new([@grid[0][1], @grid[1][1], @grid[2][1]]),
+      Set.new([@grid[0][2], @grid[1][2], @grid[2][2]]),
+      Set.new([@grid[0][0], @grid[1][1], @grid[2][2]]),
+      Set.new([@grid[0][2], @grid[1][1], @grid[2][0]])
+    ]
   end
 
   def game_board()
@@ -51,16 +50,17 @@ class Game
 
   def my_valid?(move)
     return false unless @valid_moves.include?(move)
-    return false if player_one.move.include?(move) || player_two.include?(move)
+    return false if @player_one.moves.include?(move) || @player_two.moves.include?(move)
 
     true
   end
 
   def play(player)
+    puts "#{player.name}'s turn "
     print 'Make your move e.g (A1, B2, C3): '
     move = gets.chomp.downcase
     if my_valid?(move)
-      player.moves << current_input
+      player.moves << move
     else
       puts 'Invalid move. Try again'
       play(player)
@@ -68,29 +68,39 @@ class Game
   end
 
   def won?(moves)
-    winning_cases.each do |item|
+    @winning_cases.each do |item|
       return true if item.subset?(moves)
     end
     false
   end
 
+  def game_over
+    puts "\n\n\n==== GAME OVER ==="
+    puts 'Press `p` to play again, or any other key to exit'
+    sleep(1)
+    system('clear')
+    if gets.chomp.downcase == 'p'
+      @turn = 1
+      start
+    end
+  end
+
   def start
     game_board
-    turn = 1
-
-    while turn < 9
-      current_player = turn.odd? ? @player_one : @player_two
+    tie = true
+    while @turn < 9
+      current_player = @turn.odd? ? @player_one : @player_two
       play(current_player)
 
-      if current_player.moves.length >= 3
-        if won?(current_player.moves.to_set)
-          puts "#{current_player.name} has won"
-          break
-        end
+      if current_player.moves.length >= 3 && won?(current_player.moves.to_set)
+        tie = false
+        puts "#{current_player.name} has won"
+        break
       end
       @turn += 1
-
+      puts 'OH! The game is a tie' if @turn >= 9 && tie
     end
+    game_over
   end
 end
 
@@ -99,3 +109,5 @@ player_two = Player.new('Misori', 'O')
 
 new_game = Game.new(player_one, player_two)
 new_game.start
+
+# indicate move already taken as mesage
