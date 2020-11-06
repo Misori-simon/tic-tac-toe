add2520845d065c4475e2b0fb7b5db02048e2a50
CY Kalu <cy23@tutanota.com>
2020-11-06 09:07:15 +0100

    Refactor game logic

--- lib/logic.rb
+++ lib/logic.rb
@@ -1,27 +1,11 @@
 require 'set'
 
-class Player
-  attr_reader :name, :type
-  attr_accessor :moves
-
-  def initialize(name, type)
-    @name = name
-    @type = type
-    @moves = []
-  end
-end
-
 class Game
   def initialize(player_one, player_two)
     @player_one = player_one
     @player_two = player_two
-    @valid_moves = %w[a1 a2 a3 b1 b2 b3 c1 c2 c3]
     @turn = 1
-    @grid = [
-      %w[a1 a2 a3],
-      %w[b1 b2 b3],
-      %w[c1 c2 c3]
-    ]
+    @grid = [%w[a1 a2 a3], %w[b1 b2 b3], %w[c1 c2 c3]]
     @winning_cases = [
       Set.new(@grid[0]),
       Set.new(@grid[1]),
@@ -34,80 +18,91 @@ class Game
     ]
   end
 
-  def game_board()
+  def draw_board
+    # Draws the game board to the concole
     puts '----------------'
     puts '| A1 | A2 | A3 |'
-    puts '----------------'
+    puts '-----+----+-----'
     puts '| B1 | B2 | B3 |'
-    puts '----------------'
+    puts '-----+----+-----'
     puts '| C1 | C2 | C3 |'
     puts '----------------'
   end
 
-  # def valid_moves
-  #   [*@grid[0], *@grid[1], *@grid[2]]
-  # end
+  def valid_moves
+    # Constructs an array that holds valid/allowed moves
+    # based on cells of @grid matrix
+    [*@grid[0], *@grid[1], *@grid[2]]
+  end
 
-  def my_valid?(move)
-    return false unless @valid_moves.include?(move)
-    return false if @player_one.moves.include?(move) || @player_two.moves.include?(move)
+  def move_valid?(move)
+    # Checks if move is valid cell in @grid matrix
+    valid_moves.include?(move)
+  end
+
+  def move_legal?(move)
+    # Check if selected cell has not already been taken by either player
+    return false if !move_valid?(move) or @player_one.moves.include?(move)
+    return false if !move_valid?(move) or @player_two.moves.include?(move)
 
     true
   end
 
-  def play(player)
-    puts "#{player.name}'s turn "
-    print 'Make your move e.g (A1, B2, C3): '
-    move = gets.chomp.downcase
-    if my_valid?(move)
-      player.moves << move
+  def current_player
+    # Returns supposed current_player player based on the value of @turn
+    @turn.odd? ? @player_one : @player_two
+  end
+
+  def set_turn
+    # Increments @turn in order to pass turn to other player
+    @turn += 1
+  end
+
+  def make_move
+    # Makes a move for current_player
+    puts "It's #{current_player.name}'s turn"
+    print 'Enter your move, e.g (A1, B3) => '
+
+    move = yield
+
+    if move_legal?(move)
+      current_player.moves << move
     else
-      puts 'Invalid move. Try again'
-      play(player)
+      system('clear')
+      draw_board
+      puts "\nINVALID INPUT!!!. Repeat turn."
+      make_move { yield }
     end
   end
 
-  def won?(moves)
-    @winning_cases.each do |item|
-      return true if item.subset?(moves)
+  def win?(move_set)
+    # Checks if there's a winner after move is made
+    if move_set.length >= 3
+      @winning_cases.each do |item|
+        return true if item.subset?(move_set)
+      end
     end
     false
   end
 
-  def game_over
-    puts "\n\n\n==== GAME OVER ==="
-    puts 'Press `p` to play again, or any other key to exit'
-    sleep(1)
-    system('clear')
-    if gets.chomp.downcase == 'p'
-      @turn = 1
-      start
-    end
-  end
-
   def start
-    game_board
+    # Starting point of game
     tie = true
-    while @turn < 9
-      current_player = @turn.odd? ? @player_one : @player_two
-      play(current_player)
 
-      if current_player.moves.length >= 3 && won?(current_player.moves.to_set)
+    while @turn < valid_moves.length
+      draw_board
+      make_move { yield }
+      system('clear')
+
+      if current_player.moves.length >= 3 and win?(current_player.moves.to_set)
+        puts "#{current_player.name} has won the game"
         tie = false
-        puts "#{current_player.name} has won"
         break
+      else
+        set_turn
       end
-      @turn += 1
-      puts 'OH! The game is a tie' if @turn >= 9 && tie
     end
-    game_over
+    puts 'Uh! uh!! The game is a tie' if tie
+    draw_board
   end
 end
-
-player_one = Player.new('CY', 'X')
-player_two = Player.new('Misori', 'O')
-
-new_game = Game.new(player_one, player_two)
-new_game.start
-
-# indicate move already taken as mesage
